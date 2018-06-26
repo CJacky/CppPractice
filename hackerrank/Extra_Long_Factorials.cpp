875d85c31e8082cb05f1c52175b543bbae7381be
CJacky <CJacky@users.noreply.github.com>
2018-06-27 02:23:22 +0800

    Create Extra_Long_Factorials.cpp

---
+++ hackerrank/Extra_Long_Factorials.cpp
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define SLOT_MAX 1000
+
+// Complete the extraLongFactorials function below.
+void extraLongFactorials(int n)
+{
+    vector<int> ans;
+    ans.push_back(1);
+    
+    for(int i=1; i<=n; ++i)
+    {
+        //cout<<"i="<<i<<endl;
+        for(int j=0; j<ans.size(); ++j)
+        {
+            ans[j] *= i;
+        }
+        for(int j=0; j<ans.size(); ++j)
+        {
+            if( ans[j] >= SLOT_MAX )
+            {
+                if( j == ans.size()-1 )
+                {
+                    ans.push_back(ans[j]/SLOT_MAX);
+                    ans[j] = ans[j] % SLOT_MAX;
+                }
+                else
+                {
+                    ans[j+1] += ans[j]/SLOT_MAX;
+                    ans[j] = ans[j] % SLOT_MAX;
+                }
+            }
+            //cout<<"ans["<< j <<"] = "<<ans[j]<<endl;
+        }
+    }
+    
+    printf("%d", ans.back());
+    for(int j=ans.size()-2; j>=0; --j)
+    {
+        printf("%03d", ans[j]);
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    extraLongFactorials(n);
+
+    return 0;
+}
